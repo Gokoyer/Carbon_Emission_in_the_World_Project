ebf6ec4f2f7e43a5999d244e2a9c352174aacd7a
Gokoyer <gnath2006@yahoo.com>
2023-05-24 21:48:58 +0300

    This is SQL file on carbon emission by country in 19th Century

---
+++ carbon_emission_by_country_19thCentury.sql
@@ -0,0 +1,18 @@
+SELECT TOP 25
+   Country,
+   SUM(Total) AS TotalFuel,
+   SUM([Solid Fuel]) AS SolidFuel,
+   SUM([Liquid Fuel]) AS LiquidFuel,
+   SUM([Gas Fuel]) AS GasFuel, 
+   SUM([Cement]) AS Cement,
+   SUM([Gas Flaring]) AS GasFlaring,
+   SUM([Per Capita]) AS PerCapital,
+   SUM([Bunker fuels ]) AS BunkerFuels
+FROM 
+   [dbo].[fuelsbynationscsv$]
+WHERE
+   YEAR like '18%'
+GROUP BY
+   Country
+ORDER BY
+   TotalFuel DESC
